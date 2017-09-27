8d9d3466d042dfc9b6ace22b14f60bb962f96e35
James O'Reilly <James.Oreilly926@gmail.com>
2017-09-27 14:53:43 -0400

    finished .then test specs

--- src/pledge.js
+++ src/pledge.js
@@ -6,21 +6,38 @@ Promises Workshop: build the pledge.js ES6-style promise library
 function $Promise(executor) {
   this._state = 'pending';
   this._value;
+  this._handlerGroups = []; 
+  const internalResolve = this._internalResolve.bind(this);
+  const internalReject = this._internalReject.bind(this); 
+
+  executor(function(data) {
+    internalResolve(data); 
+  }, function(reason) {
+    internalReject(reason); 
+  }); 
   if (typeof executor !== 'function') throw TypeError('executor is not a function');
 }
 
 // TODO Convert to arrow function to see how 'this' is affected.
 $Promise.prototype._internalResolve = function(someData) {
-  if (this._state != 'fulfilled') {
+  if (this._state != 'fulfilled' && this._state != 'rejected') {
     this._value = someData;
     this._state = 'fulfilled';
   }
 }
 
-$Promise.prototype._internalReject = function() {
-
+$Promise.prototype._internalReject = function(reason) {
+  if(this._state != 'rejected' && this._state != 'fulfilled') {
+    this._state = 'rejected'; 
+    this._value = reason; 
+  }
 }
 
+$Promise.prototype.then = function(successCb, errorCb) {
+  if(typeof successCb !== 'function') successCb = false; 
+  if(typeof errorCb !== 'function') errorCb = false; 
+  this._handlerGroups.push({successCb: successCb, errorCb: errorCb})
+}
 
 
 
